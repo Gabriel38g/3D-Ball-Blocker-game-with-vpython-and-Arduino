fa69d7e477b2c05d0f18b478f8703f33830d6dba
Gabriel38g <30500293+Gabriel38g@users.noreply.github.com>
2022-05-25 09:04:53 -0400

    Create JoystickControl.ino
    Arduino (Uno) code to control the Joystick for 3D Pong game.

---
+++ JoystickControl.ino
@@ -0,0 +1,31 @@
+int potX=A0;
+int potY=A1;
+int potZ=2;
+
+int xVal=0;
+int yVal=0;
+int zVal=0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(potX, INPUT);
+  pinMode(potY, INPUT);
+  pinMode(potZ, INPUT);
+  digitalWrite(potZ,HIGH);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  xVal=analogRead(potX);
+  yVal=analogRead(potY);
+  zVal=digitalRead(potZ);
+
+  Serial.print(xVal);
+  Serial.print(';');
+  Serial.print(yVal);
+  Serial.print(';');
+  Serial.println(zVal);
+  delay(50);
+}
